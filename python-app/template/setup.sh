ec90270deddf7ec72876f4edb6c9168b6c5c09f5
Chris Tseng <samfire5200@gmail.com>
2026-05-25 12:39:16 +0800

    Create Namespace

--- python-app/template/setup.sh
+++ python-app/template/setup.sh
@@ -73,6 +73,7 @@ fi
 echo ""
 echo "=== Step 1: Register self-hosted runner ==="
 kubectl config use-context docker-desktop
+kubectl create namespace "${{values.app_name}}"
 kubectl apply -f runnerdeployment.yaml
 kubectl apply -f k8s/runner-rbac.yaml
 echo "Runner registered in namespace ${{values.app_name}}."
